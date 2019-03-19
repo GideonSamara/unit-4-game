74c952a52e63c89a593f5e951a6ec8774952ed0f
Samara Gideon <sgideon1@yahoo.com>
2019-03-19 13:24:06 -0700

    unit-4-js

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+var totalScore = [];
+var wins = [];
+var losses = [];
+var Score = [];
+var crystalValue = [0, 0, 0, 0];
+
+function getScore () {
+
+    $("#score").html([Math.floor(Math.random() *120) +1])
+}
+
+});
